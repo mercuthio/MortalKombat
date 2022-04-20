d76ce229d2719de80993d00d330398d66dd73b5b
mercuthio <adrianyagohernandez@gmail.com>
2022-04-20 10:34:13 +0100

    Avanzadas hitboxes (me quiero morir salu2)

--- MortalKombat/LiuKang.h
+++ MortalKombat/LiuKang.h
@@ -5,10 +5,10 @@
 
 
 struct rectValues {
-	float width;
-	float height;
 	float left;
 	float top;
+	float width;
+	float height;
 };
 
 
@@ -170,46 +170,51 @@ static Character CreateLiuKang() {
 	animations[AnimationType::GET_CAUGHT] = getCaughtMov;
 	animations[AnimationType::CATCH] = catchMov;
 	animations[AnimationType::DYING] = dyingMov;
+	
 	/*
-	rectValues idle0[8] = {  };
-	rectValues forw0[9] = {  };
-	rectValues back0[9] = {  };
+	float left;
+	float top;
+	float width;
+	float height;
+	*/
+	/*
+	rectValues idle0[8] = { { 99,117, 72, 123}, {334,113, 70,126}, {564,111,74,130}, {797,114,74,127}, {1031,118,74,124}, {1266,114,74,126}, {1500,110,74,131}, {1739,113,71,127} };
+	rectValues forw0[9] = { {2213,110,71,131}, {2461,106,55,135}, {2696,108,56,132}, {2934,107,52,132}, {3164,111,56,128}, {3394,107,60,133}, {3868,107,53,134}, {3868,107,53,134}, {4098,112,62,128} };
+	rectValues back0[9] = { {4098,112,62,128}, {3868,107,53,134}, {3868,107,53,134}, {3394,107,60,133}, {3164,111,56,128}, {2934,107,52,132}, {2696,108,56,132}, {2461,106,55,135}, {2213,110,71,131} };
 
-	rectValues punch0[4] = {  };
-	rectValues punchUp0[4] = {  };
-	rectValues punchDown0[5] = {  };
-	rectValues punchFromUp0[5] = {  };
-	rectValues bodyToBody0[5] = {  };
-	rectValues bodyToBodyUp0[5] = {  };
+	rectValues punch0[4] = { {98,627,72,137}, {335,625,81,139}, {569,630,115,130}, {799,628,73,136} };
+	rectValues punchUp0[4] = { {}, {}, {}, {} };
+	rectValues punchDown0[5] = { {1501,2641,73,110}, {1740,2627,89,125}, {1977,2619,92,136}, {2224,2606,54,149}, {2433,2620,86,129} };
+	rectValues punchFromUp0[5] = { {2465,3591,73,84}, {2683,3592,104,78}, {2889,3589,145,82}, {3151,3592,104,77}, {3401,3591,73,84} };
+	rectValues bodyToBody0[5] = { {3608,630,84,134}, {3844,632,73,131}, {4083,634,88,128}, {4312,632,74,131}, {4544,629,84,135} };
+	rectValues bodyToBodyUp0[5] = { {}, {}, {}, {}, {} };
 
-	rectValues jump0[1] = {  };
-	rectValues jumpMove0[7] = {  };
-	rectValues jumpFall[3] = {  };
+	rectValues jump0[1] = { {103,3128,74,102} };
+	rectValues jumpMove0[7] = { {603,3607,50,70}, {833,3612,62,64}, {1059,3615,70,43}, {1293,3610,63,59}, {1544,3592,46,69}, {1768,3593,60,64}, {2013,3604,64,60} };
+	rectValues jumpFall[3] = { {3378,3127,75,102}, {3614,3130,71,142}, {3842,3154,77,119} };
 
-	rectValues kick0[7] = {  };
-	rectValues kickUp0[7] = {  };
-	rectValues kickDown0[8] = {  };
-	rectValues kickFromDown0[5] = {  };
-	rectValues kickFromUp0[5] = {  };
+	rectValues kick0[7] = { {103,1623,68,135}, {355,1620,62,133}, {591,1622,58,131}, {825,1623,109,130}, {1059,1622,57,132}, {1292,1619,61,136}, {1508,1623,65,133} };
+	rectValues kickUp0[7] = { {2418,2904,94,133}, {2671,2098,77,130}, {2934,2108,58,119}, {3173,2095,89,134}, {3410,2119,52,110}, {3629,2125,55,104}, {3850,2100,66,128} };
+	rectValues kickDown0[8] = { {1975,1623,68,133}, {2201,1667,78,89}, {2446,1696,70,65}, {2684,1702,101,62}, {2924,1701,124,60}, {3166,1696,51,89}, {3367,1654,89,102}, {3611,1622,70,135} };
+	rectValues kickFromDown0[5] = { {2914,2671,62,80}, {3151,2672,84,76}, {3385,2659,87,91}, {3618,2672,85,77}, {3850,2671,62,79} };
+	rectValues kickFromUp0[5] = { {3873,3597,78,78}, {4107,3597,90,79}, {4339,3598,110,74}, {4575,3597,91,77}, {4810,3597,77,79} };
 
-	rectValues duck0[5] = {  };
+	rectValues duck0[5] = { {99,2632,72,118}, {332,2660,76,91}, {566,2674,72,77}, {800,2660,76,90}, {1035,2631,71,118} };
 
-	rectValues blockUp0[5] = {  };
-	rectValues blockDown0[2] = {  };
+	rectValues blockUp0[5] = { {100,6172,77,132}, {331,6170,75,135}, {563,6172,74,132}, {799,6170,75,134}, {1037,6172,76,132} };
+	rectValues blockDown0[2] = { {1505,6209,68,97}, {1739,6207,69,98} };
 
-	rectValues special0[8] = {  };
-	rectValues fatality0[14] = {  };
-	rectValues win0[14] = {  };
+	rectValues special0[8] = { {86,6710,85,118}, {327,6723,78,104}, {561,6706,80,121}, {794,6694,63,134}, {1025,6697,62,130}, {1258,6699,63,130}, {1492,6699,63,129}, {1726,6698,63,130} };
+	
+	rectValues fall0[7] = { {}, {}, {}, {}, {}, {}, {} };
+	rectValues fallBack0[6] = { {}, {}, {}, {}, {}, {} };
+	rectValues fallUppercut0[8] = { {}, {}, {}, {}, {}, {}, {}, {} };
+	rectValues nuts0[4] = { {}, {}, {}, {} };
+	rectValues recover0[7] = { {}, {}, {}, {}, {}, {}, {} };
 
-	rectValues fall0[7] = {  };
-	rectValues fallBack0[6] = {  };
-	rectValues fallUppercut0[8] = {  };
-	rectValues nuts0[4] = {  };
-	rectValues recover0[7] = {  };
-
-	rectValues getCaught0[7] = {  };
-	rectValues catch0[7] = {  };
-	rectValues dying0[7] = {  };
+	rectValues getCaught0[7] = { {}, {}, {}, {}, {}, {}, {} };
+	rectValues catch0[7] = { {}, {}, {}, {}, {}, {}, {} };
+	rectValues dying0[7] = { {}, {}, {}, {}, {}, {}, {} };
 
 
 	RectangleShape rect;
